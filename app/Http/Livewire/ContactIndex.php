33ff3027200068995f7c4a797c9c979a029c4a7b
Riobush32 <riobush32@gmail.com>
2022-03-27 19:00:12 +0700

    Menambahkan Data Contact Kedalam Table

--- app/Http/Livewire/ContactIndex.php
+++ app/Http/Livewire/ContactIndex.php
@@ -7,12 +7,10 @@ use Livewire\Component;
 
 class ContactIndex extends Component
 {
- 
-    public $data;
-
     public function render()
     {
-        $this->data = Contact::latest()->get();
-        return view('livewire.contact-index');
+        return view('livewire.contact-index', [
+            'contacts' => Contact::latest()->get()
+        ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,15 +17,17 @@
 <body>
     <h1>LiveWire</h1>
 
-    <div class="card" style="width: 18rem;">
-        <ul class="list-group list-group-flush">
-            <livewire:contact-index></livewire:contact-index>
-        </ul>
-        \
+    <div class="container">
+        <div class="card" style="width: 60%; margin: 0 auto;">
+            <div class="card-header">
+                Dashboard
+            </div>
+            <div class="card-body">
+                <livewire:contact-index></livewire:contact-index>
+            </div>
+        </div>
+
     </div>
-
-
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
