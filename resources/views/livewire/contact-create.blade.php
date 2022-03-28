d192faa7dab4ec336591e8294166b718d9146e73
Riobush32 <riobush32@gmail.com>
2022-03-28 12:49:31 +0700

    Live Wire 4

---
+++ resources/views/livewire/contact-create.blade.php
@@ -0,0 +1,13 @@
+<div class="m-4">
+    <form>
+        <div class="mb-3">
+            <label for="name" class="form-label">Name </label>
+            <input type="text" class="form-control" id="name" aria-describedby="text">
+        </div>
+        <div class="mb-3">
+            <label for="phone" class="form-label">Phone</label>
+            <input type="text" class="form-control" id="phone">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
